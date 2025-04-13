d35e5a63867358c4ff9b11fc2c9d12ee4bb8b7d4
caluisen <caluisen@amazon.com>
2025-04-13 13:37:17 -0700

    index.js

--- lulu-bot/index.js
+++ lulu-bot/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const app = new App({
   token: process.env.xoxb-1226494846485-8750724387028-pgJD6edXvAPg9x6NeE6yETfE,
-  appToken: process.env.SLACK_APP_TOKEN,
+  appToken: process.env.xapp-1-A08MXP6GUGL-8764650011008-be8e11c41fd86342e1287344acc17e16f71bb2365719ff61ec52c83834c68e7d,
   socketMode: true
 });
 
